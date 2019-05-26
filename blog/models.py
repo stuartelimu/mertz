a23916c0f0e38b9e870892f0a1842311827017b5
ElimuStuart <stuartelimu@gmail.com>
2019-05-27 00:43:57 +0300

    created post_list and post_detail views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # model manager to query all published posts
 class PublishedManager(models.Manager):
@@ -29,3 +30,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
