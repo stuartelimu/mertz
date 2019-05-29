f5d5c917ddb003874ada05232e79ec5b7ec44de9
ElimuStuart <stuartelimu@gmail.com>
2019-05-30 00:15:39 +0300

    comment functionality working

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 def post_list(request):
     object_list = Post.published.all()
@@ -31,7 +31,23 @@ def post_detail(request, year, month, day, post):
                                     publish__year=year,
                                     publish__month=month,
                                     publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post':post})
+
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+    
+    if request.method == 'POST':
+        # a comment was posted
+        comment_form = CommentForm(date=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'new_comment':new_comment, 'comment_form': comment_form})
 
 def post_share(request, post_id):
     # retrieve post by id
