2fe9455d2e5203c78fde0a5e671c7fd6fc442ec3
ElimuStuart <stuartelimu@gmail.com>
2019-05-29 23:45:16 +0300

    created comments model

--- blog/models.py
+++ blog/models.py
@@ -38,3 +38,18 @@ class Post(models.Model):
             self.publish.day,
             self.slug
         ])
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
