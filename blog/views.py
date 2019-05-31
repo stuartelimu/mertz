84bb3aafe7ae2b400113fa89f3ed0fb4bf9fe7fc
ElimuStuart <stuartelimu@gmail.com>
2019-05-31 08:36:57 +0300

    added similar posts suggestion functionality

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post, Comment
@@ -18,7 +19,7 @@ def post_list(request, tag_slug=None):
 
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
-    
+
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -57,7 +58,12 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'new_comment':new_comment, 'comment_form': comment_form})
+    # list similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'new_comment':new_comment, 'comment_form': comment_form, 'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     # retrieve post by id
